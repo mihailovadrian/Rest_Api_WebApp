35b9ce5257912d0fd038ea60425bc56c724db622
mihailovadrian <adrianmihailov123@gmail.com>
2019-10-04 06:07:10 -0700

    Added suport for XML response and Refactor

--- src/main/java/com/rest/app/messenger/resources/MessageResource.java
+++ src/main/java/com/rest/app/messenger/resources/MessageResource.java
@@ -13,6 +13,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
@@ -25,11 +26,13 @@ import com.rest.app.messenger.services.MessageService;
 import com.rest.app.messenger.subresources.CommentResource;
 
 @Path("/messages")
+@Consumes(javax.ws.rs.core.MediaType.APPLICATION_JSON)
+@Produces(value = { javax.ws.rs.core.MediaType.APPLICATION_JSON,
+		MediaType.APPLICATION_XML })
 public class MessageResource {
 	MessageService messageService = new MessageService();
 
 	@GET
-	@Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
 	public List<Message> getMessages(
 			@BeanParam MessageFilterBean messageFilterBean) {
 		if (messageFilterBean.getYear() > 0)
@@ -44,7 +47,6 @@ public class MessageResource {
 
 	@GET
 	@Path("/{messageId}")
-	@Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
 	public Message getMessage(@PathParam("messageId") Long id,
 			@Context UriInfo info) {
 		Message message = messageService.getMessage(id);
@@ -56,6 +58,35 @@ public class MessageResource {
 			throw new DataNotFoundException("Message not found");
 	}
 
+
+	@POST
+	public Response addMessage(Message message, @Context UriInfo info) {
+		Message newMessage = messageService.addMessage(message);
+		String idMessage = String.valueOf(newMessage.getId());
+		URI uri = info.getAbsolutePathBuilder().path(idMessage).build();
+
+		return Response.created(uri).entity(newMessage).build();
+	}
+
+	@PUT
+	@Path("/{messageId}")
+	public Message updateMessage(@PathParam("messageId") Long id,
+			Message message) {
+		message.setId(id);
+		return messageService.updateMessage(message);
+	}
+
+	@DELETE
+	@Path("/{messageId}")
+	public Message deleteMessage(@PathParam("messageId") Long id) {
+		return messageService.deleteMessage(id);
+	}
+
+	@Path("/{messageId}/comments")
+	public CommentResource getCommentResource() {
+		return new CommentResource();
+	}
+	
 	private void addURLtoLink(UriInfo info, Message message) {
 		addURLtoSelf(info, message);
 		addURLtoProfile(info, message);
@@ -66,8 +97,7 @@ public class MessageResource {
 		String url = info.getBaseUriBuilder().path(MessageResource.class)
 				.path(MessageResource.class, "getCommentResource")
 				.path(CommentResource.class)
-				.resolveTemplate("messageId", message.getId())
-				.build()
+				.resolveTemplate("messageId", message.getId()).build()
 				.toString();
 		message.addLink(url, LinkConstants.COMMENTS);
 
@@ -86,37 +116,4 @@ public class MessageResource {
 		message.addLink(url, LinkConstants.SELF);
 	}
 
-	@POST
-	@Consumes(javax.ws.rs.core.MediaType.APPLICATION_JSON)
-	@Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
-	public Response addMessage(Message message, @Context UriInfo info) {
-		Message newMessage = messageService.addMessage(message);
-		String idMessage = String.valueOf(newMessage.getId());
-		URI uri = info.getAbsolutePathBuilder().path(idMessage).build();
-
-		return Response.created(uri).entity(newMessage).build();
-	}
-
-	@PUT
-	@Path("/{messageId}")
-	@Consumes(javax.ws.rs.core.MediaType.APPLICATION_JSON)
-	@Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
-	public Message updateMessage(@PathParam("messageId") Long id,
-			Message message) {
-		message.setId(id);
-		return messageService.updateMessage(message);
-	}
-
-	@DELETE
-	@Path("/{messageId}")
-	@Consumes(javax.ws.rs.core.MediaType.APPLICATION_JSON)
-	@Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
-	public Message deleteMessage(@PathParam("messageId") Long id) {
-		return messageService.deleteMessage(id);
-	}
-
-	@Path("/{messageId}/comments")
-	public CommentResource getCommentResource() {
-		return new CommentResource();
-	}
 }
